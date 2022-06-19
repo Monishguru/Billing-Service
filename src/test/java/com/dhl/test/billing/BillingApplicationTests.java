bdc72d0e39719ae17069c54e2c0ca2db2ff07525
Monishguru <monishguru@gmail.com>
2022-06-19 15:28:11 +0530

    Billing changes for 1.1 release jar
    Billing changes for 1.1 release jar

--- src/test/java/com/dhl/test/billing/BillingApplicationTests.java
+++ src/test/java/com/dhl/test/billing/BillingApplicationTests.java
@@ -1,13 +1,8 @@
 package com.dhl.test.billing;
 
-import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class BillingApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
 
 }
